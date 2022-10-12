3bf9a43847e56cd5efd6d02cf940a8404390413e
Micah Simmons <sonet_boy@yahoo.com>
2022-10-12 12:57:58 -0700

    HackerNoon ToDo App tutorial

---
+++ next-firebase-todo/firebase/index.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const apiKey = `${process.env.FIREBASE_API_KEY}`;
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: apiKey,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MSG_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);  
+
+export { auth, db }
---
+++ next-firebase-todo/pages/index.js
@@ -0,0 +1,13 @@
+import { Container } from "@chakra-ui/react";
+import AddTodo from "../components/AddTodo";
+import Auth from "../components/Auth";
+import TodoList from "../components/TodoList";
+export default function Home() {
+  return (
+    <Container maxW="7x1">
+      <Auth />
+      <AddTodo />
+      <TodoList />
+    </Container>
+  )
+}
