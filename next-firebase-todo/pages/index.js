06e6bd3b2db8ca357f99c3b295f3aad36e9030a4
Micah Simmons <sonet_boy@yahoo.com>
2022-10-15 11:33:11 -0700

    Added a cookie counter for class based and react hook practice

--- next-firebase-todo/pages/index.js
+++ next-firebase-todo/pages/index.js
@@ -2,12 +2,15 @@ import { Container } from "@chakra-ui/react";
 import AddTodo from "../components/AddTodo";
 import Auth from "../components/Auth";
 import TodoList from "../components/TodoList";
+import Counter from "../components/Counter";
+
 export default function Home() {
   return (
     <Container maxW="7x1">
       <Auth />
       <AddTodo />
       <TodoList />
+      <Counter />
     </Container>
   )
 }
